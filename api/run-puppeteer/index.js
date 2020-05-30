8e5b5bdfae5831eaf8c50d2ddfd724016979c912
n0bisuke <rsksound@gmail.com>
2020-05-30 22:05:38 +0900

    puppeteer

--- api/run-puppeteer/index.js
+++ api/run-puppeteer/index.js
@@ -1,41 +1,41 @@
-let chrome = {};
-let puppeteer = {};
+// let chrome = {};
+// let puppeteer = {};
 
-//puppeteer main process
-const run = async (puppeteer, chrome={}, URL='https://twitter.com/n0bisuke') => {
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  });
+// //puppeteer main process
+// const run = async (puppeteer, chrome={}, URL='https://twitter.com/n0bisuke') => {
+//   const browser = await puppeteer.launch({
+//     args: chrome.args,
+//     executablePath: await chrome.executablePath,
+//     headless: chrome.headless,
+//   });
 
-  const page = await browser.newPage();
-  await page.goto(URL);
-  // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      title: document.title,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
-  await browser.close();
+//   const page = await browser.newPage();
+//   await page.goto(URL);
+//   // Get the "viewport" of the page, as reported by the page.
+//   const dimensions = await page.evaluate(() => {
+//     return {
+//       width: document.documentElement.clientWidth,
+//       height: document.documentElement.clientHeight,
+//       title: document.title,
+//       deviceScaleFactor: window.devicePixelRatio
+//     };
+//   });
+//   await browser.close();
 
-  return dimensions;
-}
+//   return dimensions;
+// }
 
-if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
-  //Vercel
-  chrome = require('chrome-aws-lambda');
-  puppeteer = require('puppeteer-core');
-}else{
-  //Local Test
-  puppeteer = require('puppeteer');
+// if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
+//   //Vercel
+//   chrome = require('chrome-aws-lambda');
+//   puppeteer = require('puppeteer-core');
+// }else{
+//   //Local Test
+//   puppeteer = require('puppeteer');
 
-  const URL = `https://protoout.studio`;
-  run(puppeteer,{},URL).then(res => console.log(res));
-}
+//   const URL = `https://protoout.studio`;
+//   run(puppeteer,{},URL).then(res => console.log(res));
+// }
 
 module.exports = async function (context, req) {
   const id = JSON.stringify(process.env);
